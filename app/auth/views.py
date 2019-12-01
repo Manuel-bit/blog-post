a3d954bce3bcd1c091608cf97bff64b91b63f274
manuel.bit <emmanuel_njuguna@outlook.com>
2019-12-02 01:07:05 +0300

    add the login form with log in template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash
 from . import auth
 from ..models import Writter
 from .. import db
-from .forms import SignupForm
+from .forms import SignupForm,LoginForm
+from flask_login import login_user, current_user,logout_user
 
 @auth.route('/signup',methods = ["GET","POST"])
 def signup():
@@ -12,10 +13,21 @@ def signup():
     writter.password(form.password.data)
     db.session.add(writter)
     db.session.commit()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('auth.login'))
   return render_template('auth/signup.html',title='Sign Up',form=form)
 
-@main.route('/login')
+@auth.route('/login',methods = ["GET","POST"])
 def login():
-  return render_template('auth/login.html',title='Log In')
-  
+  form=LoginForm()
+  if form.validate_on_submit():
+    writter=Writter.query.filter_by(username = form.username.data).first()
+    if writter != None and writter.verify_password(form.password.data):
+      login_user(writter,form.remember.data)
+      return redirect(url_for('main.index'))
+    flash('invalid username or password')
+  return render_template('auth/login.html',title='Log In',form=form)
+
+@auth.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('main.index'))
